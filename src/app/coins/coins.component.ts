8772e2afbb0f539614f230dca00e7fdcb4d08379
Alexandre Julien <alexandre.jln@protonmail.com>
2023-10-27 15:29:28 +0200

    save 1.1

--- src/app/coins/coins.component.ts
+++ src/app/coins/coins.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CoinsService } from "../coins.service";
 
 @Component({
   selector: 'app-coins',
   templateUrl: './coins.component.html',
   styleUrls: ['./coins.component.css']
 })
-export class CoinsComponent {
+export class CoinsComponent implements OnInit {
   coins: any;
 
+  constructor(private coinsService: CoinsService) {}
+
+  ngOnInit() {
+    this.coinsService.getCoins().subscribe((data) => {
+
+      console.log(data)
+    })
+  }
 }
